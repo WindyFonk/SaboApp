76aebef0d4871d1a8484e7253a4d83261d5ebd1e
Tran Vu Phong <phongteobeo@gmail.com>
2022-11-01 18:55:28 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private int a;
-    private String alpha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
