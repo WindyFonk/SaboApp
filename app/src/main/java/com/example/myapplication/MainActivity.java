cd24060c1b97fb9ed409cbc9a0ba4dcbf415e5fa
Hanjiv1 <revolutionvvvv@gmail.com>
2022-11-03 18:46:56 +0700

    Logo

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,10 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView ivLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+                //Ánh xạ
+        ivLogo = findViewById(R.id.ivLogo);
+
+        //Sử dụng Glide with là với trang nào, load: dữ liệu nào, into: id của imageview
+        Glide.with(this).load(R.mipmap.logo).into(ivLogo);
+
+        //Sử dụng handler để tạo ra delay và dùng cách tạo activity nhanh để chuyển giao diện
+        // new Handler().postDelayed(new Runnable() {
+        //     @Override
+        //     public void run() {
+        //         startActivity(new Intent(MainActivity.this,HomeActivity.class));
+        //     }
+        // },4000);
+
     }
 }
