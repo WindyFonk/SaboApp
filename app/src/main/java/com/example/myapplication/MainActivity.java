66de5e574b420360c3e39ce5d9cb6d9d3b1c6d63
Tran Vu Phong <phongteobeo@gmail.com>
2022-11-05 15:42:40 +0700

    Glide

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
     ImageView ivLogo;
