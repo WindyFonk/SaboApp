ab74d9af1afb68bfdc1227884b526dc033761594
Hanjiv1 <revolutionvvvv@gmail.com>
2022-11-09 10:32:24 +0700

    navigation drawer

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,17 +1,30 @@
+
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.splashscreen.adapter.FeaturedAdapter;
 import com.example.splashscreen.adapter.FeaturedHelperClass;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     RecyclerView featuredRecycler;
     RecyclerView.Adapter adapter;
+    ImageView menuIcon;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,21 +32,58 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         featuredRecycler = findViewById(R.id.featured_recycler);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        menuIcon = findViewById(R.id.ivMenu);
+
 
         featuredRecycler();
+        navigationDrawer();
+
+    }
+
+    private void navigationDrawer() {
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                else drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+        super.onBackPressed();
     }
 
     private void featuredRecycler() {
         featuredRecycler.setHasFixedSize(true);
-        featuredRecycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        featuredRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
         ArrayList<FeaturedHelperClass> featuredLocations = new ArrayList<>();
 
-        featuredLocations.add(new FeaturedHelperClass(R.drawable.fitness_new,"Want a perfect body? Read this","Copyright by author"));
-        featuredLocations.add(new FeaturedHelperClass(R.drawable.girl,"New Style","Copyright by author"));
-        featuredLocations.add(new FeaturedHelperClass(R.drawable.cats,"Dayly dose of cat","Copyright by author"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.fitness_new, "Want a perfect body? Read this", "Copyright by author"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.girl, "New Style", "Copyright by author"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.cats, "Dayly dose of cat", "Copyright by author"));
 
         adapter = new FeaturedAdapter(featuredLocations);
         featuredRecycler.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.Profile:
+            startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+            break;
+        }
+        return true;
     }
 }
