e46299875678408857b80d6c1b3969f8a59daf18
Tran Vu Phong <phongteobeo@gmail.com>
2022-11-12 12:16:06 +0700

    Profile activity (load info)

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -9,16 +9,27 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.adapter.FeaturedAdapter;
 import com.example.adapter.FeaturedHelperClass;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     RecyclerView featuredRecycler;
@@ -26,6 +37,10 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     ImageView menuIcon;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    TextView Name, Address;
+    CircleImageView Pfp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +49,47 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
         featuredRecycler = findViewById(R.id.featured_recycler);
         drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.navigation_view);
+        navigationView = findViewById(R.id.navigation_view_customer);
         menuIcon = findViewById(R.id.ivMenu);
+
+        //getting Side navigation
+        View headerView = navigationView.getHeaderView(0);
+        Name = headerView.findViewById(R.id.user_name_side);
+        Address=headerView.findViewById(R.id.address_side);
+        Pfp=headerView.findViewById(R.id.pfpside);
+
+        //getting user id
+        SharedPreferences sharedPreferences =
+                getSharedPreferences("USER",MODE_PRIVATE);
+        String id = sharedPreferences.getString("Id",null);
+
+        DocumentReference docRef = db.collection("AppUsers").document(id);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        //get and set user's info
+                        String name = (String) document.get("name");
+                        String address = (String) document.get("address");
+                        String image = (String) document.get("image");
+
+                        Name.setText(name);
+                        Address.setText(address);
+                        Glide.with(HomeActivity.this)
+                                .load(image)
+                                .into(Pfp);
+                        Log.d(">>TAG", name +"\n"+address+"\n"+image);
+                    } else {
+                        Log.d("TAG", "No such document");
+                    }
+                } else {
+                    Log.d("TAG", "get failed with ", task.getException());
+                }
+            }
+        });
+
 
 
         featuredRecycler();
@@ -46,11 +100,17 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     private void navigationDrawer() {
         navigationView.bringToFront();
         navigationView.setNavigationItemSelectedListener(this);
+
+        //User information on navigation side
+
+
+
         menuIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                if (drawerLayout.isDrawerVisible(GravityCompat.START)){
                     drawerLayout.closeDrawer(GravityCompat.START);
+                }
                 else drawerLayout.openDrawer(GravityCompat.START);
             }
         });
--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -1,17 +1,73 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.myapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
+    TextView Name, Address, Phonenumber, Email;
+    CircleImageView Pfppic;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Name=findViewById(R.id.pfname);
+        Address=findViewById(R.id.pfaddess);
+        Phonenumber=findViewById(R.id.pfphone);
+        Email=findViewById(R.id.pfemail);
+        Pfppic=findViewById(R.id.pfppic);
+        loadInfo();
+    }
+
+    private void loadInfo(){
+        SharedPreferences sharedPreferences =
+                getSharedPreferences("USER",MODE_PRIVATE);
+        String id = sharedPreferences.getString("Id",null);
+
+        DocumentReference docRef = db.collection("AppUsers").document(id);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        //get and set user's info
+                        String name = (String) document.get("name");
+                        String address = (String) document.get("address");
+                        String image = (String) document.get("image");
+                        String phonenumber = (String) document.get("phonenumber");
+                        String email= (String) document.get("email");
+                        Name.setText(name);
+                        Address.setText(address);
+                        Phonenumber.setText(phonenumber);
+                        Email.setText(email);
+                        Glide.with(ProfileActivity.this)
+                                .load(image)
+                                .into(Pfppic);
+                        Log.d(">>TAG", name +"\n"+address+"\n"+image);
+                    } else {
+                        Log.d("TAG", "No such document");
+                    }
+                } else {
+                    Log.d("TAG", "get failed with ", task.getException());
+                }
+            }
+        });
     }
 }
