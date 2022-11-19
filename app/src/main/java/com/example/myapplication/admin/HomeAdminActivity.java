10b5166e84bed9f80895044a5fdf62af9babc2fa
Hanjiv1 <revolutionvvvv@gmail.com>
2022-11-19 23:18:09 +0700

    admin_layout

--- app/src/main/java/com/example/myapplication/admin/HomeAdminActivity.java
+++ app/src/main/java/com/example/myapplication/admin/HomeAdminActivity.java
@@ -1,13 +1,107 @@
 package com.example.myapplication.admin;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class HomeAdminActivity extends AppCompatActivity {
+import com.example.adapter.FeaturedAdapter;
+import com.example.adapter.FeaturedHelperClass;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+
+public class HomeAdminActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    RecyclerView featuredRecycler;
+    RecyclerView.Adapter adapter;
+    ImageView menuIcon;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Button Edit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_admin);
+        setContentView(R.layout.activity_home);
+
+        featuredRecycler = findViewById(R.id.featured_recycler);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        menuIcon = findViewById(R.id.ivMenu);
+        View headerView = navigationView.getHeaderView(0);
+        Edit = headerView.findViewById(R.id.btnEdit);
+
+
+        featuredRecycler();
+        navigationDrawer();
+
+        Edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeAdminActivity.this, ManagerActivity.class));
+            }
+        });
+
     }
+
+    private void navigationDrawer() {
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                else drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    @SuppressLint("SuspiciousIndentation")
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
+    private void featuredRecycler() {
+        featuredRecycler.setHasFixedSize(true);
+        featuredRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        ArrayList<FeaturedHelperClass> featuredLocations = new ArrayList<>();
+
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.banner1, "It'll fit, trust us", ""));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.banner2, "New Style", ""));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.banner3, "Newly fashion trend", ""));
+
+        adapter = new FeaturedAdapter(featuredLocations);
+        featuredRecycler.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Profile:
+                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                break;
+
+            case R.id.shop:
+                startActivity(new Intent(getApplicationContext(), Shop.class));
+                break;
+        }
+        return true;
+    }
+
 }
