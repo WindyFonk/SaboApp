fde6d4087c306875aee93bab8da8a88f3d37b21c
Hanjiv1 <revolutionvvvv@gmail.com>
2022-11-19 22:39:14 +0700

    admin

---
+++ app/src/main/java/com/example/myapplication/admin/HomeAdminActivity.java
@@ -0,0 +1,13 @@
+package com.example.myapplication.admin;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_admin);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/admin/ManagerActivity.java
@@ -0,0 +1,13 @@
+package com.example.myapplication.admin;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager);
+    }
+}
